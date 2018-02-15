d0ce40cf4654e423877b78c5c06ab967b20db381
nguyenadrien <nguyen_adrien@hotmail.fr>
2018-02-15 11:18:19 +0100

    player et gameconsole

---
+++ Awale/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awale
+{
+    class Player
+    {
+        private int score;
+        private String name;
+
+        public int Score { get => score; set => score = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Player(int score, string name)
+        {
+            this.score = score;
+            this.name = name;
+        }
+    }
+}
